ac227764ff6117963b57721728c57efff4879db7
dunkelgrau <42265994+dunkelgrau@users.noreply.github.com>
2025-01-19 11:13:25 +0000

    fragenanordnung

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ if st.button("Fertig"):
         x=alt.X('Häufigkeit:Q', title='Häufigkeit'),  # Häufigkeit entlang der X-Achse
         y=alt.Y('Frage:N', title='Frage', axis=alt.Axis(labelAngle=0)),  # Fragen entlang der Y-Achse
         color='Antwort:N',  # Farbliche Codierung je Antwort (Smiley)
-        row='Frage:N',  # Die Antworten werden nach Frage gruppiert, nicht nach Smiley
+        row='Antwort:N',  # Smileys werden jetzt untereinander innerhalb der Fragen gruppiert
         tooltip=['Frage:N', 'Antwort:N', 'Häufigkeit:Q']
     ).properties(
         title="Antworten zur Umfrage",
